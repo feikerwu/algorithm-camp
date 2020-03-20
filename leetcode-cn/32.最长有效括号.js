2d4b8230861f3e4cab2ff175ee454ba5262b74a1
feikerwu <feikerwu@gmail.com>
2020-03-20 12:17:23 +0800

    feat: 32

---
+++ leetcode-cn/32.最长有效括号.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=32 lang=javascript
+ *
+ * [32] 最长有效括号
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestValidParentheses = function(s) {
+  let stack = new Array()
+  let longest = 0
+  stack.push(-1)
+  for(let i = 0; i < s.length; i++) {
+    if (s[i] === '(') {
+      stack.push(i)
+    } else {
+      stack.pop()
+      if (stack.length === 0) {
+        stack.push(i)
+      } else {
+        longest = Math.max(longest, i - stack[stack.length - 1])
+      }
+    }
+  }
+  return longest
+};
+// @lc code=end
+
