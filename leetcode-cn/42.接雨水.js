13bc3043009ff41714587ddbda1bf6811bbf7f77
feikerwu <feikerwu@gmail.com>
2020-04-02 17:26:55 +0800

    feat: 4 => 暴力求解

---
+++ leetcode-cn/42.接雨水.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=42 lang=javascript
+ *
+ * [42] 接雨水
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    
+};
+// @lc code=end
+
