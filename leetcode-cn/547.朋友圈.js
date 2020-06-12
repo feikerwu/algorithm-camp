c71b6f19b5476bc552a9bcf3a729e192f2233732
feikerwu <feikerwu@gmail.com>
2020-06-12 14:20:53 +0800

    feat: 547(BFS/DFS/并查集)

--- leetcode-cn/547.朋友圈.js
+++ leetcode-cn/547.朋友圈.js
@@ -33,27 +33,56 @@
 // };
 
 // BFS
-var findCircleNum = function (M) {
-  const visited = Array(M.length).fill(0);
-  let res = 0;
-  const queue = [];
-  for (let i = 0; i < M.length; i++) {
-    if (!visited[i]) {
-      visited[i] = 1;
-      res++;
-      queue.push(i);
-    }
+// var findCircleNum = function (M) {
+//   const visited = Array(M.length).fill(0);
+//   let res = 0;
+//   const queue = [];
+//   for (let i = 0; i < M.length; i++) {
+//     if (!visited[i]) {
+//       visited[i] = 1;
+//       res++;
+//       queue.push(i);
+//     }
 
-    while (queue.length) {
-      const cur = queue.shift();
-      for (let j = 0; j < M.length; j++) {
-        if (cur !== j && M[cur][j] && !visited[j]) {
-          queue.push(j);
-          visited[j] = 1;
-        }
+//     while (queue.length) {
+//       const cur = queue.shift();
+//       for (let j = 0; j < M.length; j++) {
+//         if (cur !== j && M[cur][j] && !visited[j]) {
+//           queue.push(j);
+//           visited[j] = 1;
+//         }
+//       }
+//     }
+//   }
+//   return res;
+// };
+
+var findCircleNum = function (M) {
+  let count = M.length;
+  let parnets = Array.from(M).map((item, index) => index);
+  function find(x) {
+    if (parnets[x] === x) {
+      return x;
+    }
+    return (parnets[x] = find(parnets[x]));
+  }
+
+  function union(x, y) {
+    if (find(x) === find(y)) {
+      return;
+    }
+    parnets[parnets[x]] = parnets[y];
+    count--;
+  }
+
+  for (let i = 0; i < M.length; i++) {
+    for (let j = i + 1; j < M[i].length; j++) {
+      if (M[i][j]) {
+        union(i, j);
       }
     }
   }
-  return res;
+
+  return count;
 };
 // @lc code=end
