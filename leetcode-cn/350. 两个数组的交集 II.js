739a3ce7dff38023fdcb0977fc58041f00b77520
feikerwu <feikerwu@gmail.com>
2020-07-13 00:18:58 +0800

    feat: 677.双指针.js

---
+++ leetcode-cn/350. 两个数组的交集 II.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function (nums1, nums2) {
+  nums1.sort((a, b) => a - b);
+  nums2.sort((a, b) => a - b);
+  let i = 0,
+    j = 0,
+    res = [];
+  while (i < nums1.length && j < nums2.length) {
+    if (nums1[i] === nums2[j]) {
+      res.push(nums1[i]);
+      i++;
+      j++;
+    } else if (nums1[i] > nums2[j]) {
+      j++;
+    } else {
+      i++;
+    }
+  }
+  return res;
+};
