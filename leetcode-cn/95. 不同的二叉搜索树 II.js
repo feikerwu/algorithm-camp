945fa3bc3ee251c0c24914bbbcfd1b2bf77b379a
feikerwu <feikerwu@gmail.com>
2020-07-21 10:35:59 +0800

    feat: 二叉搜索树的数量

--- leetcode-cn/95. 不同的二叉搜索树 II.js
+++ leetcode-cn/95. 不同的二叉搜索树 II.js
@@ -4,8 +4,8 @@ function TreeNode(val) {}
  * @return {TreeNode[]}
  */
 let memo = {
-  0: [[null]],
-  1: [[1]],
+  0: [],
+  1: [new TreeNode(1)],
 };
 var generateTrees = function (n) {
   if (memo[n]) {
@@ -15,21 +15,31 @@ var generateTrees = function (n) {
   for (let i = 1; i <= n; i++) {
     let left = generateTrees(i - 1);
     let right = generateTrees(n - i);
+    left.length === 0 && (left = [null]);
+    right.length === 0 && (right = [null]);
     for (let leftTree of left) {
       for (let rightTree of right) {
-        res.push([i, ...leftTree, ...cloneTree(rightTree, i)]);
+        let curRoot = new TreeNode(i);
+        curRoot.left = leftTree;
+        curRoot.right = cloneTree(rightTree, i);
+        res.push(curRoot);
       }
     }
   }
   memo[n] = res;
-
   return res;
 };
 
-function cloneTree(arr, offset) {
-  return arr.map((item) => (item === null ? null : item + offset));
+function cloneTree(node, offset) {
+  if (node === null) {
+    return null;
+  }
+  let newNode = new TreeNode(node.val + offset);
+  newNode.left = cloneTree(node.left, offset);
+  newNode.right = cloneTree(node.right, offset);
+  return newNode;
 }
 
-generateTrees(3);
-console.log(memo[3]);
+// generateTrees(3);
+// console.log(memo[3]);
 // console.log(memo);
