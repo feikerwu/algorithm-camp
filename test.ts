42e7b8ff815b4bb7984655d4b19dee0f407f2f88
feikerwu <feikerwu@gmail.com>
2021-07-19 17:14:37 +0800

    feat: daily

--- test.ts
+++ test.ts
@@ -1,20 +1,25 @@
-const remainder = 10 ** 9 + 7;
+function maxFrequency(nums: number[], k: number): number {
+  nums.sort((a, b) => a - b);
 
-function countPairs(deliciousness: number[]): number {
-  let map = new Map<number, number>();
-  let res = 0;
-  for (let food of deliciousness) {
-    for (let i = 0; i <= 22; i++) {
-      const pair = (1 << i) - food;
-      if (pair > 0 && map.has(pair)) {
-        res = (res + map.get(pair)) % remainder;
-      }
+  let start = 0,
+    end = start + 1,
+    result = -Infinity;
+
+  let total = 0;
+  while (start <= end && end < nums.length) {
+    // 向右边每移动一位，都会提供 (nums[end] - nums[end - 1]) * (end - start) 步的贡献
+    total = total + (nums[end] - nums[end - 1]) * (end - start);
+    while (total > k) {
+      // 左窗口移动，左窗口每移动一位，都会减少(右窗口到左窗口个步数)
+      total = total - (nums[end] - nums[start]);
+      start++;
     }
 
-    map.set(food, (map.get(food) || 0) + 1);
+    result = Math.max(result, end - start + 1);
+    end++;
   }
 
-  return res;
+  return result;
 }
 
-countPairs([1, 3, 5, 7, 9]);
+console.log(maxFrequency([3, 9, 6], 2));
